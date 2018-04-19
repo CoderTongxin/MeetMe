02c6bbd2d458f6f6af63ddf948311e47e383dfda
CoderTongxin <gin199413@163.com>
2018-04-19 23:40:06 +1200

    add input boxes to signup page
    add more input boxes to signup page to get user's information, and add
    user's info to firebase database, also move styles from login & signup
    page as a const object

---
+++ app/const/styles.js
@@ -0,0 +1,43 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    container : {
+        backgroundColor:'#f4f4f4',
+        flex: 1,
+        alignItems:'center',
+        justifyContent :'center'
+    },
+    textContent : {
+        flexGrow: 1,
+        alignItems:'flex-end',
+        justifyContent :'center',
+        paddingVertical:16,
+        flexDirection:'row'
+    },
+    text: {
+        color:'#000000',
+        fontSize:16
+    },
+    textLink: {
+        color:'#616161',
+        fontSize:16,
+        fontWeight:'500'
+    },
+    inputContainer : {
+        flexGrow: 1,
+        justifyContent:'center',
+        alignItems: 'center'
+    },
+
+    inputBox: {
+        width: 280,
+        backgroundColor: '#c1c1c1',
+        borderRadius: 20,
+        paddingHorizontal: 16,
+        fontSize: 18,
+        color: '#000000',
+        marginVertical: 10,
+        height: 25
+    }
+
+});
--- app/pages/Login.js
+++ app/pages/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet,
     Text,
     View,
     TextInput,
@@ -12,6 +11,7 @@ import Logo from '../components/Logo';
 import Loader from '../components/Loader';
 import SubmitButton from '../components/SubmitButton';
 import {Actions} from 'react-native-router-flux';
+import {styles} from "../const/styles";
 
 export default class Login extends Component {
 
@@ -55,64 +55,26 @@ export default class Login extends Component {
                     <TextInput style={styles.inputBox}
                                underlineColorAndroid='rgba(0,0,0,0)'
                                placeholder="Email"
-                               placeholderTextColor = "#ffffff"
-                               selectionColor="#fff"
+                               placeholderTextColor = "#000000"
+                               selectionColor="#000"
                                keyboardType="email-address"
                         onChangeText={(text) => this.setState({email:text})}
                     />
                     <TextInput style={styles.inputBox}
                                underlineColorAndroid='rgba(0,0,0,0)'
+                               selectionColor="#000"
                                placeholder="Password"
                                secureTextEntry={true}
-                               placeholderTextColor = "#ffffff"
+                               placeholderTextColor = "#000000"
                         onChangeText={(text) => this.setState({password:text})}
                     />
                     <SubmitButton onPress={()=> this.login()} type='Login'/>
                 </View>
                 <View style={styles.textContent}>
                     <Text style={styles.text}>Don't have an account yet?</Text>
-                    <TouchableOpacity onPress={this._goToSignUp}><Text style={styles.textLink}> Signup</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={this._goToSignUp}><Text style={styles.textLink}>  Signup</Text></TouchableOpacity>
                 </View>
             </View>
         )
     }
 }
-const styles = StyleSheet.create({
-    container : {
-        backgroundColor:'#455a64',
-        flex: 1,
-        alignItems:'center',
-        justifyContent :'center'
-    },
-    textContent : {
-        flexGrow: 1,
-        alignItems:'flex-end',
-        justifyContent :'center',
-        paddingVertical:16,
-        flexDirection:'row'
-    },
-    text: {
-        color:'rgba(255,255,255,0.6)',
-        fontSize:16
-    },
-    textLink: {
-        color:'#ffffff',
-        fontSize:16,
-        fontWeight:'500'
-    },
-    inputContainer : {
-        flexGrow: 1,
-        justifyContent:'center',
-        alignItems: 'center'
-    },
-
-    inputBox: {
-        width: 300,
-        backgroundColor: 'rgba(255, 255,255,0.2)',
-        borderRadius: 25,
-        paddingHorizontal: 16,
-        fontSize: 16,
-        color: '#ffffff',
-        marginVertical: 10
-    }
-});
