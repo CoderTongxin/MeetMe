dd6bcfc28c5aa16da2708bd623fa6f50588293aa
PQ <qpen546@aucklanduni.ac.nz>
2018-04-20 16:41:01 +1200

    Map Integration: Add google map integration, there are 3 main functionality added, get user recent location, show the database locations and send the current location to database.

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet,  View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Routes from './app/Routes';
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-          <Routes/>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Routes/>
+            </View>
+        );
+    }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
--- app/Routes.js
+++ app/Routes.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Router, Stack, Scene} from 'react-native-router-flux';
 
 import Login from './pages/Login';
 import SignUp from './pages/Signup';
 import userProfile from './pages/UserProfile'
+import home from './pages/Home'
 
-export default class Routes extends Component {
+export default class Routs extends React.Component {
     render() {
-        return(
+        return (
             <Router>
                 <Stack key="root" hideNavBar={true}>
-                    <Scene key="login" component={Login} title="Login" initial={true}/>
+                    <Scene key="login" component={Login} title="Login"/>
                     <Scene key="signUp" component={SignUp} title="Register"/>
                     <Scene key="userProfile" component={userProfile} title="UserProfile"/>
+                    <Scene key="userProfile" component={userProfile} title="UserProfile"/>
+                    <Scene key="home" component={home} title="UserProfile" initial={true}/>
                 </Stack>
             </Router>
         )
