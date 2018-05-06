923222746db580c1ebf18dad68a914ba7d848931
CoderTongxin <gin199413@163.com>
2018-05-07 00:00:30 +1200

    fix delete&quit activity function

--- app/components/ActivityList.js
+++ app/components/ActivityList.js
@@ -74,7 +74,6 @@ export default class ActivityList extends React.Component {
         });
         this.setState({
             showDetail: !this.state.showDetail,
-            activity:null
         });
         Promise.all(participantsList)
             .then(() => {
@@ -89,8 +88,7 @@ export default class ActivityList extends React.Component {
     quitActivity() {
         firebaseRef.database().ref('activities/' + this.state.activityKey + '/participants/' + this.props.user.uid).remove().then(() => {
             this.setState({
-                showDetail: !this.state.showDetail,
-                activity:null
+                showDetail: !this.state.showDetail
             });
             firebaseRef.database().ref('users/' + this.props.user.uid + '/activities/' + this.state.activityKey).remove()
         })
